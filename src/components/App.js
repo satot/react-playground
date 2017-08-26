6a719a3131b21a992e0274d4d015d3bb4876b482
satot <ph1.ph2.sa25@gmail.com>
2017-08-26 19:41:48 +0800

    Implemented rename function (specification 5)

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
 import List, { ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
+
 import FileFolder from 'material-ui/svg-icons/file/folder';
 import DocumentIcon from 'material-ui/svg-icons/editor/insert-drive-file';
-import {blue500} from 'material-ui/styles/colors';
+import MoreVert from 'material-ui/svg-icons/navigation/more-vert';
+import { transparent, blue500, grey400 } from 'material-ui/styles/colors';
 
 export default class App extends React.Component {
     constructor (props) {
@@ -14,12 +19,28 @@ export default class App extends React.Component {
     }
 
     componentWillMount () {
+        // make sure modal is closed when page has been changed
+        if (this.props.open) {
+            this.props.handleCloseModal();
+        }
+    }
+
+    componentDidMount () {
         let params = this.props.match.params;
         if(params && params.folderId) {
             this.props.getResourcesByFolderAsync(params.folderId);
         } else {
             this.props.getResourcesAsync();
         }
+    }
+
+    handleTextFieldChange(e) {
+        this.props.handleChangeName(e.target.value);
+    }
+
+    handleRenameSubmit() {
+        this.props.handleRenameSubmit();
+        this.props.handleCloseModal();
     }
 
     render () {
@@ -30,8 +51,10 @@ export default class App extends React.Component {
                     key={'f' + f.id.toString()}
                     id={f.id}
                     name={f.name}
+                    open={this.props.open}
                     description={f.description}
                     handleTouch={this.props.getResourcesByFolderAsync}
+                    onMenuClick={this.props.handleOpenModal}
                 />
             );
         }) : null;
@@ -42,12 +65,37 @@ export default class App extends React.Component {
                     id={d.id}
                     name={d.name}
                     updated_at={d.updated_at}
+                    onMenuClick={this.props.handleOpenModal}
                 />
             );
         }) : null;
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                onClick={this.props.handleCloseModal}
+            />,
+            <FlatButton
+                label="Rename"
+                primary={true}
+                onClick={this.handleRenameSubmit.bind(this)}
+            />,
+        ];
         return (
             <div>
                 <List>{folders} {documents}</List>
+                <Dialog
+                    title="Rename"
+                    actions={actions}
+                    modal={false}
+                    open={this.props.open}
+                    onRequestClose={this.props.handleCloseModal}
+                >
+                    <TextField
+                        id="tf1"
+                        value={this.props.file.name}
+                        onChange={this.handleTextFieldChange.bind(this)}
+                    />
+                </Dialog>
             </div>
         );
     }
@@ -58,13 +106,31 @@ class Folder extends React.Component {
         this.props.handleTouch(folderId);
     }
 
+    handleMenu (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute('data-file-id');
+        const name = e.currentTarget.getAttribute('data-file-name');
+        this.props.onMenuClick({id: id, name: name, type: 'folder'});
+    }
+
     render() {
         let linkTo = '/' + this.props.id.toString();
         return (
             <ListItem
                 key={this.props.id}
                 leftAvatar={<Avatar icon={<FileFolder />} />}
-                onTouchTap={this.handleTouchTap.bind(this, this.props.id)}
+                rightAvatar={
+                    <Avatar
+                        icon={<MoreVert />}
+                        backgroundColor={transparent}
+                        color={grey400}
+                        onClick={this.handleMenu.bind(this)}
+                        data-file-id={this.props.id}
+                        data-file-name={this.props.name}
+                    />
+                }
+                onClick={this.handleTouchTap.bind(this, this.props.id)}
                 containerElement={<Link to={linkTo} />}
                 primaryText={this.props.name}
                 secondaryText={this.props.description}
@@ -74,12 +140,30 @@ class Folder extends React.Component {
 }
 
 class Document extends React.Component {
+    handleMenu (e) {
+        e.stopPropagation();
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute('data-file-id');
+        const name = e.currentTarget.getAttribute('data-file-name');
+        this.props.onMenuClick({id: id, name: name, type: 'document'});
+    }
+
     render() {
         let updt = new Date(this.props.updated_at);
         return (
             <ListItem
                 key={this.props.id}
                 leftAvatar={<Avatar icon={<DocumentIcon />} backgroundColor={blue500} />}
+                rightAvatar={
+                    <Avatar
+                        icon={<MoreVert />}
+                        backgroundColor={transparent}
+                        color={grey400}
+                        onClick={this.handleMenu.bind(this)}
+                        data-file-id={this.props.id}
+                        data-file-name={this.props.name}
+                    />
+                }
                 primaryText={this.props.name}
                 secondaryText={dtFormat(updt.getFullYear(), updt.getMonth(), updt.getDate())}
             />
--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,9 @@
 const initialState = {
-    files: {}
+    open: false,
+    file: {},
+    files: {},
+    // to store renamed files as mocking api response
+    renamedFiles: []
 };
 
 
@@ -9,9 +13,51 @@ export default function reducer(state = initialState, action) {
     }
     switch(action.type) {
     case 'GET_RESOURCES':
-        return assign({ files: action.files});
+        var files = action.files;
+        state.renamedFiles.forEach((f) => { renameFiles(files, f); });
+        return assign({ files: files });
+    case 'MODAL_OPEN':
+        return assign({ open: true, file: action.file });
+    case 'MODAL_CLOSE':
+        return assign({ open: false, file: {} });
+    case 'CHANGE_NAME':
+        return assign({
+            file: Object.assign({}, state.file, {name: action.name})
+        });
+    case 'RENAME_SUBMIT':
+        state.renamedFiles.push(state.file);
+        return assign({ files: renameFiles(state.files, state.file) });
     default:
         return state;
     }
 }
 
+// Find renamed file in files and rename if any
+function renameFiles(files, file) {
+    switch(file.type) {
+    case 'folder':
+        var folders = files.folders;
+        if (folders) {
+            folders.map((f) => {
+                if(f.id == file.id) {
+                    f.name = file.name;
+                }
+                return f;
+            });
+        }
+        return Object.assign({}, files, {folders: folders});
+    case 'document':
+        var documents = files.documents;
+        if (documents) {
+            documents.map((d) => {
+                if(d.id == file.id) {
+                    d.name = file.name;
+                }
+                return d;
+            });
+        }
+        return Object.assign({}, files, {documents: documents});
+    default:
+        return files;
+    }
+}
